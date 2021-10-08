033fad8b9a261319ad9e65a4cb8b518fe58eb413
Alex Harvill <alex@colrspace.com>
2021-10-08 15:50:44 -0700

    mock additional modules + submodules

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,12 @@ import os
 import sys
 import unittest.mock
 
-MOCK_MODULES = ['tensorflow']
+MOCK_MODULES = [
+    'tensorflow',
+    'tensorflow.keras',
+    'tensorflow.keras.backend',
+    'tensorflow_probability.python.internal',
+]
 for mod_name in MOCK_MODULES:
   sys.modules[mod_name] = unittest.mock.Mock()
 
